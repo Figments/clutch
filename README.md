650faa07e2578d208f2d04bddbe78b32a5342b77
Landon Mote <figments@outlook.com>
2019-12-12 12:49:35 -0800

    Added more to main lib

---
+++ README.md
@@ -0,0 +1,9 @@
+# Clutch
+
+Clutch is a MongoDB ODM inspired by MongooseJS and Rust's very own 
+Diesel ORM. Its goal is to provide a means of interfacing with MongoDB 
+in an easy to use, intuitive way, without sacrificing the ability to 
+perform complex queries.
+
+This project is ostensibly a Work In Progress, and large portions of the 
+API are subject to change.
--- clutch/src/lib.rs
+++ clutch/src/lib.rs
@@ -1,7 +1,22 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+//! # Clutch
+//! 
+//! Clutch is a MongoDB ODM inspired by MongooseJS and Rust's very own 
+//! Diesel ORM. Its goal is to provide a means of interfacing with MongoDB 
+//! in an easy to use, intuitive way, without sacrificing the ability to 
+//! perform complex queries.
+//! 
+//! This project is ostensibly a Work In Progress, and large portions of the 
+//! API are subject to change.
+
+/* MongoDB */
+extern crate mongodb;
+#[macro_use] extern crate bson;
+
+/* Serde */
+extern crate serde;
+extern crate serde_json;
+#[macro_use] extern crate serde_derive;
+
+/* Sys */
+extern crate ron;
+#[macro_use] extern crate bitflags;
